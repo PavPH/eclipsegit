1a78763c3e6ec5b2c1a236986c5c4bc2ace1e132
PavPH <pavanpadma23@gmail.com>
2022-07-09 09:53:54 +0530

    SampleMain class modified

--- SampleProject/src/SampleMain.java
+++ SampleProject/src/SampleMain.java
@@ -2,7 +2,9 @@
 public class SampleMain {
 
 	public static void main(String[] args) {
+		System.out.println("--------------------------------");
 		System.out.println("Hello ");
+		System.out.println("-----------------------------------");
 
 	}
 
