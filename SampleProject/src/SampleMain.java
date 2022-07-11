97b3e935df516170bc3bf6152e5381a39f099a1e
PAVANHEBBAR23 <PAVAN8095564053@GMAIL.COM>
2022-07-11 10:37:44 +0530

    changes in SampleMain class

--- SampleProject/src/SampleMain.java
+++ SampleProject/src/SampleMain.java
@@ -4,6 +4,7 @@ public class SampleMain {
 	public static void main(String[] args) {
 		System.out.println("--------------------------------");
 		System.out.println("Hello ");
+		System.out.println("this editing is from collabarator");
 		System.out.println("-------------------------------------");
 
 	}
